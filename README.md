c63494bbc6d9a54fb3fbddb4686bc3e48fdc3fe3
api-haus <yuri@api.haus>
2025-09-07 19:58:44 +0300

    fix: unity 6000

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Melon Cinematic Tonemapper
+
 An efficient Tonemapper to fix ACES Issues - For Unity &amp; Prowl Game Engines
 
 ![LTMX_Melon_Banner_Thin](https://raw.githubusercontent.com/ltmx/Melon-Tonemapper/main/.branding/LTMX_Melon_Banner_Thin.jpeg)
 
 # Motivations
-ACES is broken - High intensity colors shift hues
-- Intense blue shifts to purple, making highlights dull and dissappear completely in some cases,
-- Intense Reds shift to Yellow, providing a warmer feeling, but making things appear as they have some black body radiation.
 
-Melon tonemapping fixes that by shifting the color to white at high intensities, 
+ACES is broken - High intensity colors shift hues
+
+- Intense blue shifts to purple, making highlights dull and dissappear completely in some cases,
+- Intense Reds shift to Yellow, providing a warmer feeling, but making things appear as they have some black body
+	radiation.
+
+Melon tonemapping fixes that by shifting the color to white at high intensities,
 with a slight "controlable" hue shift for a more pleasing look:
 Green and red slightly shift to yellow and blue shifts to cyan before going to white.
 
@@ -17,17 +21,31 @@ Contrast is ajusted to approximate ACES levels
 # Supported Platforms
 
 ### Unity Engine
+
 - Currently only supports High Definition Render Pipeline 14.0.3 and above, using a fullscreen shader graph
 - Will add support for Universal Render Pipeline and Post Processing Stack Package.
 
 ### Prowl Engine
+
 - Implemented natively
 - See : https://github.com/michaelsakharov/Prowl
-
 
 # Screenshots
 
 ![ACES VS MELON](https://github.com/ltmx/Melon-Tonemapper/assets/47640688/ba8a07db-a037-428c-8ce9-eb311f29535e)
 
 # Why "Melon" you might ask !?
+
 - My last name is Reinhard but Reinhard tonemapping already exists, so I just picked an old pseudonym I go by ;)
+
+---
+
+# fork changelist
+
+fork: api-haus/Melon-Tomemaper
+
+- added versionDefine to auto-declare `UNITY_HDRP` if hdrp package present
+- fixed issues with unity 6000 rendering
+	- fixed greyscreen issue - fixed missing shader blocks for cmax, remap01
+	- fixed hdrp dynamic resolution
+- added package.json for direct from git url installation
--- Unity/MelonTonemapping.cs
+++ Unity/MelonTonemapping.cs
@@ -1,4 +1,5 @@
 ﻿#region Header
+
 // **    Copyright (C) 2023 Nicolas Reinhard, @LTMX. All rights reserved.
 // **    Github Profile: https://github.com/LTMX
 // **    Repository : https://github.com/LTMX/Unity.Athena
@@ -7,47 +8,70 @@
 
 #if UNITY_HDRP
 
-using System;
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.HighDefinition;
+namespace Melon_Tonemapper.Unity
+{
+	using System;
+	using UnityEngine;
+	using UnityEngine.Rendering;
+	using UnityEngine.Rendering.HighDefinition;
 
-    [Serializable, SupportedOnRenderPipeline(typeof(HDRenderPipelineAsset))]
-    internal sealed class MelonTonemapping : CustomPostProcessVolumeComponent, IPostProcessComponent
-    {
-        internal class ShaderIDs
-        {
-            // public static readonly int k_InputTexture = Shader.PropertyToID("_InputTexture");
-            public static readonly int Contrast = Shader.PropertyToID("_Contrast");
-            public static readonly int WhiteIntensity = Shader.PropertyToID("_WhiteIntensity");
-        }
+	[Serializable]
+	[SupportedOnRenderPipeline(typeof(HDRenderPipelineAsset))]
+	sealed class MelonTonemapping : CustomPostProcessVolumeComponent, IPostProcessComponent
+	{
+		public BoolParameter isActive = new(true, true);
 
-        public BoolParameter isActive = new(true, true);
+		[Tooltip("Default to 0.15")]
+		public ClampedFloatParameter WhiteIntensity = new(0.15f, 0, 1, true);
 
-        [Tooltip("Default to 0.15")]
-        public ClampedFloatParameter WhiteIntensity = new(0.15f, 0, 1, true);
-        [Tooltip("Default to 0.64")]
-        public ClampedFloatParameter Contrast = new(0.64f, 0, 1, true);
- 
-        
-        Material m_Material;
-        
-        public bool IsActive() => m_Material != null && isActive.value;
+		[Tooltip("Default to 0.64")]
+		public ClampedFloatParameter Contrast = new(0.64f, 0, 1, true);
 
-        public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+		Material m_Material;
 
-        public override void Setup() => m_Material = CoreUtils.CreateEngineMaterial("FullScreen/MelonTonemapping");
+		public override CustomPostProcessInjectionPoint injectionPoint =>
+			CustomPostProcessInjectionPoint.AfterPostProcess;
 
-        public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
-        {
-            Debug.Assert(m_Material != null);
+		public bool IsActive()
+		{
+			return m_Material != null && isActive.value;
+		}
 
-            m_Material.SetFloat(ShaderIDs.Contrast, Contrast.value);
-            m_Material.SetFloat(ShaderIDs.WhiteIntensity, WhiteIntensity.value); 
-            HDUtils.DrawFullScreen(cmd, m_Material, destination);
-        }
+		public override void Setup()
+		{
+			m_Material = CoreUtils.CreateEngineMaterial("FullScreen/MelonTonemapping");
+		}
 
-        public override void Cleanup() => CoreUtils.Destroy(m_Material);
-    }
-    
-    #endif
+		public override void Render(
+			CommandBuffer cmd,
+			HDCamera camera,
+			RTHandle source,
+			RTHandle destination
+		)
+		{
+			Debug.Assert(m_Material != null);
+
+			m_Material.SetFloat(ShaderIDs.Contrast, Contrast.value);
+			m_Material.SetFloat(ShaderIDs.WhiteIntensity, WhiteIntensity.value);
+			m_Material.SetTexture("_MainTex", source);
+			//_MainTex is a hacky way to explicitly include support for dynamic resolution, as unity's documentation suggests
+			//can't care enough to look at their source codes to for a non-hacky way - it works
+
+			HDUtils.DrawFullScreen(cmd, m_Material, destination);
+		}
+
+		public override void Cleanup()
+		{
+			CoreUtils.Destroy(m_Material);
+		}
+
+		internal class ShaderIDs
+		{
+			// public static readonly int k_InputTexture = Shader.PropertyToID("_InputTexture");
+			public static readonly int Contrast = Shader.PropertyToID("_Contrast");
+			public static readonly int WhiteIntensity = Shader.PropertyToID("_WhiteIntensity");
+		}
+	}
+}
+
+#endif
